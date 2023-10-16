cae46c7d5ee1339c390bdac606a1101085a5344d
Murat Aznabakiyev <m.aznabakiyev@petrel.ai>
2023-10-17 02:18:35 +0600

    feat(message-list-item): background hover added

--- src/components/molecules/MessageListItem.tsx
+++ src/components/molecules/MessageListItem.tsx
@@ -19,7 +19,7 @@ export const MessageListItem: FC<TMessageListItemProps> = ({
 }) => {
   return (
     <div
-      className={`px-10 py-[20px] h-[90px] border-b-[1px] border-[#9AACB5] cursor-pointer ${listItemBg}`}
+      className={`px-10 py-[20px] h-[90px] border-b-[1px] border-[#9AACB5] cursor-pointer hover:bg-white-gray duration-300 ${listItemBg}`}
     >
       <div className="flex justify-between items-center">
         <p className="text-[18px] font-[500] leading-[28px]">{title}</p>
