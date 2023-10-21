f11f1d02a7990fbe4ee67c51c79681ad478efdfa
Murat Aznabakiyev <m.aznabakiyev@petrel.ai>
2023-10-21 21:49:59 +0600

    feat: icons generator added

---
+++ src/components/hooks/useWindowSize.ts
@@ -0,0 +1,24 @@
+// useWindowSize.js
+import { useState, useEffect } from "react";
+
+export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState<{
+    width: number;
+    height: number;
+  }>();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+};
--- src/components/organisms/ChatBox.tsx
+++ src/components/organisms/ChatBox.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { FC } from "react";
 import { RecipientBar } from "@/components/molecules/RecipientBar";
 import { ChatContainer } from "@/components/molecules/ChatContainer";
 
-export const ChatBox = () => {
+type TChatBoxProps = {
+  onBack?: () => void;
+};
+
+export const ChatBox: FC<TChatBoxProps> = ({ onBack }) => {
   return (
     <div className="flex-grow w-full bg-primaryChat flex flex-col justify-between">
-      <RecipientBar />
+      <RecipientBar onBack={onBack} />
       <ChatContainer />
     </div>
   );
